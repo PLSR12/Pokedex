5ee09cf5c2762aea4f2db5e9a5212e77b78566e1
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-03-24 11:25:18 -0300

    lintagem dos pokemons e config Api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
+import Pokedex from "./components/Pokedex";
+import { getPokemons } from "./Api";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const result = await getPokemons();
+      setPokemons(result);
+      setLoading(false);
+    } catch (error) {
+      console.log("fetchPokemons error:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("carregou");
+    fetchPokemons();
+  } ,[]);
+
   return (
     <div>
       <Navbar />
-      <Searchbar/>
-      <div className="App"></div>
+      <Searchbar />
+      <Pokedex pokemons={pokemons.results} loading={loading}  />
     </div>
   );
 }
