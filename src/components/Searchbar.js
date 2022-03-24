3d1f53d7ee781383d17f53bfd5626f273ac0144b
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-03-24 09:03:44 -0300

    initial commit

---
+++ src/components/Searchbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Searchbar = () => {
+    return (
+        <div>
+            <div>
+                <input type="search" placeholder="Buscar Pokemon" />
+            </div>
+        </div>
+    )
+}
+
+export default Searchbar
