bd26d0727d10c482f5ce49f7aee3c6a7b792ac17
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-03-24 10:12:07 -0300

    integration Api and render Pokemon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="App"> </div>
+      <Searchbar/>
+      <div className="App"></div>
     </div>
   );
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import {searchPokemon} from "../Api"
 
 const Searchbar = () => {
-    return (
-        <div>
-            <div>
-                <input type="search" placeholder="Buscar Pokemon" />
-            </div>
-        </div>
-    )
-}
+  const [search, setSearch] = useState("dito");
+  const [pokemon, setPokemon] = useState();
 
-export default Searchbar
+  const onChangeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onSearchHandler = async (pokemon) => {
+    const result = await searchPokemon(pokemon);
+    setPokemon(result);
+  };
+
+  const onButtonClickHandler = () => {
+    onSearchHandler(search);
+  };
+
+  return (
+    <div className="searchbar-container">
+      <div className="searchbar">
+        <input
+          type="search"
+          placeholder="Buscar Pokemon"
+          onChange={onChangeHandler}
+        />
+      </div>
+      <div className="searchbar-btn">
+        <button onClick={onButtonClickHandler}> Buscar </button>
+      </div>
+      {pokemon ? (
+        <div> 
+          <div>
+            <div> Nome: {pokemon.name}</div>
+            <div> Peso: {pokemon.weight}</div>
+            <img src={pokemon.sprites.front_default} alt="pokemon.name" />
+          </div>
+        </div>
+    
+      ) : null}
+    </div>
+  );
+};
+
+export default Searchbar;
